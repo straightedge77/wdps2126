1c2f72e51a47e7602df663eb12f23ace05b31f90
Ric0rdanza <Ric0rdanza@github.com>
2021-11-26 06:54:31 -0800

    Add run.sh

--- helper.py
+++ helper.py
@@ -1,11 +1,12 @@
 import sys
 
 if __name__ == "__main__":
+    output = sys.argv[1]
     folder = "temp"
     lines = []
     with open(folder + "/part-00000") as rf:
         lines = rf.readlines()
-    with open("./result.tsv", "w+") as wf:
+    with open("./" + output + ".tsv", "w+") as wf:
         for line in lines:
             if line == "None\n":
                 continue
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+python3 entity_linking.py $1
+python3 helper.py $2
